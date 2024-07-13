36a7de2a01592f2c46e639e947f37722c5cdf7eb
choryang <okee0815@gmail.com>
2024-07-13 14:54:27 +0900

    Feat: Create Filter

--- src/pages/settings/Stat.jsx
+++ src/pages/settings/Stat.jsx
@@ -7,13 +7,27 @@ import StatItem from "src/components/StatItem";
 function Stat() {
 
   const [statList, setStatList] = useState([]);
+  const [statTypeList, setStatTypeList] = useState([]);
+  const [type, setType] = useState("");
 
-  var type = "";
+  const handleSelect = async (e) => {
+    var code = e.target.value;
+    if(code < 40) { 
+      setType("");
+    } else {
+      setType(code - 40);
+    }
+
+    const response = await axios.get(`/api/stat/list?statType=${type}`);
+    setStatList(response.data);
+  }
 
   useEffect(() => {
     axios.get(`/api/stat/list?statType=${type}`).then((res) => {
-      console.log(res);
       setStatList(res.data);
+    })
+    axios.get(`/api/common/code?type=4`).then((res) => {
+      setStatTypeList(res.data);
     })
   }, [])
 
@@ -23,6 +37,14 @@ function Stat() {
     <Header />
     <div className="mainContainer">
         <div className="pageTitle">스탯 관리</div>
+        <select onChange={handleSelect}>
+          <option value={39}>전체</option>
+          {statTypeList.map((item) => (
+            <option value={item.code} key={item.code}>
+              {item.codeName}
+            </option>
+          ))}
+        </select>
         <div className="listContainer">
         {statList && statList.map((statItem, index) => {
             return (
